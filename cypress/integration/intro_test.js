d3eb13ae2fdc340ab0c5d07d21f4cd6b9f3144e4
vyslouzilr <vyslouzilr@magexo.cz>
2021-10-11 21:20:19 +0200

    Remove Cypress example stuff

---
+++ cypress/integration/intro_test.js
@@ -0,0 +1,41 @@
+import * as testFixtures from '../fixtures/test_fixtures.json'
+
+describe('MageXo intro test', () => {
+  beforeEach(() => {
+    cy.visit(testFixtures.env.homepage)
+  })
+
+  context('Newsletter', () => {
+    it('Sign for a newsletter', () => {
+      cy.log('remove before flight')
+    })
+  })
+
+  context('Shopping cart', () => {
+    it('Add an item to shopping cart', () => {
+      cy.log('remove before flight')
+    })
+
+    it('Remove item from shopping cart', () => {
+      cy.log('remove before flight')
+    })
+
+    it('Place order', () => {
+      cy.log('remove before flight')
+    })
+  })
+
+  context('Search bar', () => {
+    it('Search for an item and open it', () => {
+      cy.log('remove before flight')
+    })
+    it('Filter brand while searching', () => {
+      cy.log('remove before flight')
+    })
+
+    it('Filter price while searching', () => {
+      cy.log('remove before flight')
+    })
+  })
+
+})
